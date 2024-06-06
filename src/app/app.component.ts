01e321fd40c0f7d92d110e4c86542f2804dbae4b
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2024-06-06 12:19:52 +0100

    add

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
